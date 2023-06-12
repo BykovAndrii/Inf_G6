e9189d77fd70283d8736bef04cbf3180869ba0fe
Andrii Bykov <Andrii.Bykov@infopulse.com>
2023-06-12 19:07:04 +0300

    Add project files.

---
+++ Inf_G6_Git/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inf_G6_Git
+{
+    public class Class1
+    {
+    }
+}
