75ea4fa63647d4f033708ef5b169ae8e817cc1de
Andrii Bykov <Andrii.Bykov@infopulse.com>
2023-06-12 19:22:13 +0300

    First commit.

--- Inf_G6_Git/Class1.cs
+++ Inf_G6_Git/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Inf_G6_Git
+﻿namespace Inf_G6_Git
 {
     public class Class1
     {
